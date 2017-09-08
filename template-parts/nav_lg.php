662febb1a020d3c6f95c993f5b2f7f691598e286
nutanek <nutcs@hotmail.com>
2017-09-09 02:28:32 +0700

    add dynamic nav colors

--- template-parts/nav_lg.php
+++ template-parts/nav_lg.php
@@ -1,15 +1,14 @@
 <nav class="row headernav--lg justify-content-center">
     <div class="col-md-11">
         <ul class="row justify-content-center font-theme">
-            <li class="headernav--lg__menu">
+            <li class="headernav--lg__menu --color-0">
                 <?php echo __indy('home'); ?>
             </li>
-            <li class="headernav--lg__menu -dotted">
+            <?php for ($i=1; $i<5; $i++) : ?>
+            <li class="headernav--lg__menu --dotted --color-<?php echo ($i%5); ?>">
                 <?php echo __indy('home'); ?>
             </li>
-            <li class="headernav--lg__menu -dotted">
-                <?php echo __indy('home'); ?>
-            </li>
+            <?php endfor; ?>
         </ul>
     </div>
 </nav>
